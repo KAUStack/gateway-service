4701114522b00ca2eb15f74f7dfef7c7d8b6cdd7
Yasser Alghamdi <86261957+iYasserGh@users.noreply.github.com>
2026-02-24 22:32:39 +0300

    Implement Profile-based CORS configurations (#2)
    * Configurate CORS
    
    * Implement Profile-based CORS configurations for dev and prod environments
    
    * create application-dev-properties and configure profiles in the properties files
    
    * update CORS configuration to use dynamic allowed origins from properties

--- src/main/java/com/kaustack/gateway/config/SecurityConfig.java
+++ src/main/java/com/kaustack/gateway/config/SecurityConfig.java
@@ -7,12 +7,22 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Arrays;
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, 
                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
@@ -20,9 +30,30 @@ public class SecurityConfig {
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
     }
 
+    /**
+    * CORS configuration with stricter rules
+    * @return CorsConfigurationSource with CORS rules applied
+    **/
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public CorsConfigurationSource CorsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Rules
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        // Set rules on paths
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, CorsConfigurationSource corsConfigurationSource) {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/auth/**").permitAll()
